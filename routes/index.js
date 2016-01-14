07c6a82c363fa959e8f574b3d02a8516ac8f6e8c
John Curry <john.steven.curry@gmail.com>
2016-01-13 19:10:38 -0800

    high scores printing but not in order

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var db = req.db;
+  var collection = db.get('highScores');
+  collection.find({}, {}, function(e, docs){
+    res.render('index', {
+      "highScores": docs
+    });
+  });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
