b8ba8142919aa50e6073db7dba5a5fcc8aeb816a
John Curry <john.steven.curry@gmail.com>
2016-01-17 14:31:43 -0800

    using a sort function to sort top 10 high scores

---
+++ README.md
@@ -0,0 +1,6 @@
+
+To-do, things to think about:
+
+- Do I even need a "sort" function? Wouldn't it be better to sort the highscore objects at the database level and then just grab the top 10?
+- Learn Jade better, the current template is hideous, and only because my lack of knowledge of Jade
+
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,27 @@ router.get('/', function(req, res, next) {
   var db = req.db;
   var collection = db.get('highScores');
   collection.find({}, {}, function(e, docs){
+    console.log(docs);
+    console.log("clear clear clear");
+    docs.sort(compareObjs);
     res.render('index', {
       "highScores": docs
     });
   });
   //res.render('index', { title: 'Express' });
 });
+
+
+//This function should probably NOT be here, I just don't know where to put it yet.
+function compareObjs(a, b){
+  if (a.score > b.score){
+    return -1;
+  } else if ( a.score < b.score){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 
 module.exports = router;
